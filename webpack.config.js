ccd60f7d3aeac58ebd796d86fff3a66c8f22f9e7
yuko <yuko@example.com>
2018-12-20 11:13:22 +0300

    lesson 12

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,22 @@ module.exports = {
 		rules: [
 			{
 				test:  /\.js$/,
+				exclude: /node_modules/,
 				use: {
-					loader: 'babel-loader'
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true,
+					}
 				}
 
+			},
+			
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				]
 			}
 		]
 	},
