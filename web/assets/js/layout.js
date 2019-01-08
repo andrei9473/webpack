fc4af131450b9ad035acc53913d931c4020d552d
yuko <yuko@example.com>
2019-01-08 16:47:21 +0300

    15

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -2,7 +2,10 @@
 
 const $ = require('jquery');
 require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
 require('babel-polyfill');
+require('../css/main.css');
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, 'web', 'build'),
-		filename: '[name].js'
+		filename: '[name].js',
+		publicPath: '/build/'
 
 	},
 	module: {
@@ -32,6 +33,26 @@ module.exports = {
 					'style-loader',
 					'css-loader',
 				]
+			},
+
+			{
+				test: /\.(svg|png|jpg|gif)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name]-[hash:6].[ext]'
+					}
+				}
+			},
+
+			{
+				test: /\.(woff|woff2|ttf|eot|otf)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name]-[hash:6].[ext]'
+					}
+				}
 			}
 		]
 	},
